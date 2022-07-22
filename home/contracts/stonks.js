93cb0d44625c4b742f0f4b39993fa40ff3912897
BuildTools <unconfigured@null.spigotmc.org>
2022-07-22 17:40:34 +0200

    Added function for contract Stock trader III

--- home/contracts/stonks.js
+++ home/contracts/stonks.js
@@ -11,3 +11,31 @@ export function tradeI(arr) {
 export function tradeII(arr) {
     //TBD
 }
+
+export function tradeIII(arr) {
+    let first = 0;
+    let firstIndex = [0,0];
+    for(let buy of arr) {
+        for (let sell of arr.slice(arr.indexOf(buy))) {
+            if (sell - buy > first) {
+                firstIndex = [arr.indexOf(buy), arr.indexOf(sell)];
+                first = sell - buy;
+            }
+        }
+    }
+    let arr1 = arr.slice(0, firstIndex[0]);
+    let arr2 = arr.slice(firstIndex[1]);
+
+    let second = 0;
+    for (let buy of arr1) {
+        for (let sell of arr1.slice(arr1.indexOf(buy))) {
+            second = Math.max(second, sell - buy);
+        }
+    }
+    for (let buy of arr2) {
+        for (let sell of arr2.slice(arr2.indexOf(buy))) {
+            second = Math.max(second, sell - buy);
+        }
+    }
+    return first + second;
+}

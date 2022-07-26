ba1f6e02e546630927bfdeec08be15b20da15ac4
BuildTools <unconfigured@null.spigotmc.org>
2022-07-26 21:37:28 +0200

    Fixed, last integer being ignored.

--- home/contracts/maxSum.js
+++ home/contracts/maxSum.js
@@ -1,7 +1,7 @@
 function maxSum(input){
     let bSum = input[0];
     for(let i = 0; i < input.length; i++){
-        for(let j = i + 1; j < input.length; j++){
+        for(let j = i + 1; j <= input.length; j++){
             let arr = input.slice(i,j);
             let sum = arr.reduce((a,b) => (a + b));
             bSum = Math.max(bSum, sum);

8d6d5820cca492e0369a887fb195c58ce935c48c
BuildTools <unconfigured@null.spigotmc.org>
2022-07-26 20:22:17 +0200

    Added fibonacci formula.

--- home/metrics.js
+++ home/metrics.js
@@ -1,4 +1,18 @@
 /** @param {NS} ns */
 export async function main(ns, a) {
-    ns.disableLog('ALL');
+    let t = parseInt(ns.args[0]);
+    const num = isNaN(t) ? 25 : parseInt(ns.args[0]);
+    ns.tprint(fibonacci(num));
+}
+
+/**
+ * Returns the i-th number in the fibonacci sequence.
+ * @param {number} i -th number to find. 
+ */
+function fibonacci(i) {
+    const sqr5 = Math.sqrt(5);
+    const osqr5 = 1 / sqr5;
+    const a = osqr5 * (Math.pow((1 + sqr5) / 2, i + 1));
+    const b = osqr5 * (Math.pow((1 - sqr5) / 2, i + 1));
+    return Math.floor(a - b); // In reality no flooring required, FPU f**ks this up...
 }

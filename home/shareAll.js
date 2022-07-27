1a84530e707f281029c4b8c60201ac71f4da8025
BuildTools <unconfigured@null.spigotmc.org>
2022-07-27 18:47:24 +0200

    Formatted from showing decimal to percentage

--- home/shareAll.js
+++ home/shareAll.js
@@ -13,8 +13,8 @@ import {getAllServers} from './contractFinder';
         if (threads == 0 || !ns.hasRootAccess(name)) continue;
         sum += threads;
         await ns.scp('sharing.js', name);
-        ns.tprint(`sharing on ${name} with ${threads} threads.`)
-            ns.exec('sharing.js', name, threads);
+        ns.tprint(`sharing on ${name} with ${threads} threads.`);
+        ns.exec('sharing.js', name, threads);
     }
     let power = 1 + Math.log(sum) / 25;
     ns.tprint(`Total threads: ${sum}, resulting in a power of ${(power * 100).toFixed(2)}%`)

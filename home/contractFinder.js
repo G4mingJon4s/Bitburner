5edcfe3d4f7cd348425b711e9d4de1bc07d9c925
BuildTools <unconfigured@null.spigotmc.org>
2022-07-29 05:46:24 +0200

    Added table compression and spacing toggleing

--- home/contractFinder.js
+++ home/contractFinder.js
@@ -50,53 +50,15 @@ function getAllContracts(ns, servers, red = true) {
 }
 
 /**
- * @param {import('../.vscode/NetscriptDefinitions').NS} ns 
- */
-function log(ns, table) {
-    let length1 = 0;
-    let length2 = 0;
-    let names = [];
-    let values = [];
-    let rows = [];
-    for(let data of table) {
-        length1 = length1 > data.name.length ? length1 : data.name.length;
-        length2 = length2 > data.contracts.toString().replace(',',', ').length ? length2 : data.contracts.toString().replace(',',', ').length;
-    }
-    for(let data of table) {
-        let name = data.name.padEnd(length1,' ');
-        let value = data.contracts.toString().replace(',',', ').padEnd(length2, ' ');
-        names.push(name);
-        values.push(value);
-    }
-    let head = "┌";
-    head = head.padEnd(length1 + 1, '-');
-    head += '┬';
-    head = head.padEnd(head.length + length2, '-');
-    head += '┐';
-    let butt = "└";
-    butt = butt.padEnd(length1 + 1, '-');
-    butt += '┴';
-    butt = butt.padEnd(butt.length + length2, '-');
-    butt += '┘';
-    rows.push(head);
-    for(let i = 0; i < names.length; i++){
-        let name = names[i];
-        let value = values[i];
-        rows.push('|' + name + '|' + value + '|');
-    }
-    rows.push(butt);
-    for(let row of rows){
-        ns.tprint(row);
-    }
-}
-
-/**
  * Creates a table for the given entries.
  * @param {string[][]} values A 2-dimensional array, where each array represents one column.
  * @param {boolean} sep Wether or not the table should have row divisors.
+ * @param {boolean} compress Wether or not to compress the table to one string.
+ * @param {boolean} spaces Wether or not to add spaces between lines, if `compress` is true.
  * @returns A string aray, ready to be printed.
  */
-export function table(values, div = false) {
+export function table(values, div = false, compress = false, spaces = true) {
+    const timePad = ''.padEnd(22, ' ');
     let style = DefaultStyle();
 
     let col = values[0].length;
@@ -132,6 +94,11 @@ export function table(values, div = false) {
         }
         out.push(butt);
         return out;
+   }
+   if (compress) {
+        let result = [head, ...lines, butt];
+        let string = result.reduce((a, b) => (a.join(spaces ? '\n' + spaces + b : '\n' + b)));
+        return string;
    }
 
     return [head, ...lines, butt];

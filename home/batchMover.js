b987e213716e2c854ec91fb2127bb097b77a43a7
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 12:55:17 +0200

    added autocomplete and refactoring

--- home/batchMover.js
+++ home/batchMover.js
@@ -1,6 +1,19 @@
 /**@param {import('../.vscode/NetscriptDefinitions').NS} ns */
 export async function main(ns) {
-    let target = ns.args[0];
-    const files = ['c-weaken.js','c-grow.js','c-hack.js','batchController.js','batchUtil.js','sendJson.js','lambert.js'];
-    await ns.scp(files, 'home', target);
+  let target = ns.args[0];
+  const files = [
+    "c-weaken.js",
+    "c-grow.js",
+    "c-hack.js",
+    "batchController.js",
+    "batchUtil.js",
+    "sendJson.js",
+    "lambert.js",
+    "resize.js",
+  ];
+  await ns.scp(files, "home", target);
+}
+
+export function autocomplete(data, args) {
+  return [...data.servers];
 }

afbdf8d08cf61723a969ea6d9d321850a0844a49
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 12:52:32 +0200

    added auto connecting and refactoring

--- home/findServer.js
+++ home/findServer.js
@@ -1,53 +1,55 @@
-import {getAllServers} from './contractFinder';
-
 /**
- * @param {import('../.vscode/NetscriptDefinitions').NS} ns 
+ * @param {import('../.vscode/NetscriptDefinitions').NS} ns
  */
- export async function main(ns) {
-    let target = ns.args[0];
-    if (target == 1) {
-        for(let name of await getAllServers(ns)){
-            ns.tprint(name);
-        }
-        return;
-    }
-    if (target == 2) {
-        for (let name of await getAllServers(ns)){
-            if (ns.hasRootAccess(name)) ns.tprint(name);
-        }
-        return;
-    }
-    try {ns.getServer( target); } catch { ns.alert(`Target is not a valid server!`); return; }
-    let route = await findServer(ns, target);
-    ns.tprint(convertToConnect(route));
+export async function main(ns) {
+  const target = ns.args[0];
+  const mode = ns.args[1] ?? true;
+  try {
+    ns.getServer(target);
+  } catch {
+    ns.alert(`Target is not a valid server!`);
+    return;
+  }
+  let route = await findServer(ns, target);
+
+  if (mode) {
+    let terminalIn = document.getElementById("terminal-input");
+    terminalIn.value = convertToConnect(route);
+    const handler = Object.keys(terminalIn)[1];
+    terminalIn[handler].onChange({
+      target: terminalIn,
+    });
+    terminalIn[handler].onKeyDown({
+      key: "Enter",
+      preventDefault: () => null,
+    });
+    return;
+  }
+  ns.tprint(convertToConnect(route));
 }
 
 /**
- * @param {import('../.vscode/NetscriptDefinitions').NS} ns 
+ * @param {import('../.vscode/NetscriptDefinitions').NS} ns
+ * @param {String} current
  * @param {String} target
+ * @returns {String[] | undefined}
  */
-export async function findServer(ns, target) {
-    /**
-     * @param {import('../.vscode/NetscriptDefinitions').NS} ns 
-     * @param {String} current
-     * @param {String} target
-     * @returns {String[] | undefined}
-     */
-    async function perm(ns, current, target, path = []) {
-        await ns.sleep(1);
-        path.push(current);
-        if (path.includes(target)) return path;
-        let next = await ns.scan(current);
-        next = next.filter(s => !path.includes(s));
-        if (next.length == 0) return undefined;
-        for (let s of next) {
-            let res = await perm(ns, s, target, [...path]);
-            if (typeof res !== "undefined") {
-                return res;
-            }
-        }
+export async function findServer(ns, target, current = "home", path = []) {
+  await ns.sleep(1);
+
+  path.push(current);
+  if (path.includes(target)) return path;
+
+  let next = await ns.scan(current);
+  next = next.filter((s) => !path.includes(s));
+  if (next.length == 0) return undefined;
+
+  for (let s of next) {
+    let res = await findServer(ns, target, s, [...path]);
+    if (typeof res !== "undefined") {
+      return res;
     }
-    return await perm(ns, 'home', target);
+  }
 }
 
 /**
@@ -56,22 +58,23 @@ export async function findServer(ns, target) {
  * @returns {String} printed array
  */
 export function convertToConnect(arr) {
-    let out = '';
-    let prefix = 'connect ';
-    let suffix = '; ';
-    for(let name of arr) {
-        if (name == 'home') {
-            out += name;
-            out += suffix;
-            continue;
-        }
-        out += prefix;
-        out += name;
-        out += suffix;
+  let out = "";
+  let prefix = "connect ";
+  let suffix = "; ";
+  for (let name of arr) {
+    if (name == "home") {
+      out += name;
+      out += suffix;
+      continue;
     }
-    return out;
+    out += prefix;
+    out += name;
+    out += suffix;
+  }
+  return out;
 }
 
+/**@param {import('../.vscode/NetscriptDefinitions').AutocompleteData} data */
 export function autocomplete(data, args) {
-    return [...data.servers];
+  return [...data.servers, "true", "false"];
 }

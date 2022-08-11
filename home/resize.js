47b6109c14105e5eeadf0e4222f933ef50449930
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 12:51:09 +0200

    added resizing as function

--- home/resize.js
+++ home/resize.js
@@ -1,23 +1,42 @@
 /** @param {import('../.vscode/NetscriptDefinitions').NS} ns */
 export async function main(ns) {
-    let name = ns.args[0];
-    let width = ns.args[1] ?? 400;
-    let height = ns.args[2] ?? 400;
+  let name = ns.args[0];
+  let width = ns.args[1] ?? 400;
+  let height = ns.args[2] ?? 400;
 
-    resizeTail(name, width, height);
+  resizeTail(name, width, height);
 
-    //name=script name
-    function resizeTail(name, width, height) {
-        for (const tail of globalThis['document'].querySelectorAll(`h6[title*="${name}"]`))
-            if (tail.parentNode.parentNode.className == 'react-resizable') tail.parentNode.parentNode.setAttribute('style', `width: ${width}px; height: ${height}px;`)
-    }
+  //name=script name
+  function resizeTail(name, width, height) {
+    for (const tail of globalThis["document"].querySelectorAll(
+      `h6[title*="${name}"]`
+    ))
+      if (tail.parentNode.parentNode.className == "react-resizable")
+        tail.parentNode.parentNode.setAttribute(
+          "style",
+          `width: ${width}px; height: ${height}px;`
+        );
+  }
 }
 
 /** @param {import('../.vscode/NetscriptDefinitions').NS} ns */
 export function newWindow(ns, width, height) {
-    let doc = eval("document");
-    ns.tail();
-    let logArea = doc.querySelector(".react-draggable:last-child .react-resizable:last-child");
-    logArea.style.width = (width + "px");
-    logArea.style.height = (height + "px");
+  let doc = eval("document");
+  ns.tail();
+  let logArea = doc.querySelector(
+    ".react-draggable:last-child .react-resizable:last-child"
+  );
+  logArea.style.width = width + "px";
+  logArea.style.height = height + "px";
+}
+
+export function resizeWindow(name, width, height) {
+  for (const tail of globalThis["document"].querySelectorAll(
+    `h6[title*="${name}"]`
+  ))
+    if (tail.parentNode.parentNode.className == "react-resizable")
+      tail.parentNode.parentNode.setAttribute(
+        "style",
+        `width: ${width}px; height: ${height}px;`
+      );
 }
